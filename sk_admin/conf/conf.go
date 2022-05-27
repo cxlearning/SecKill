bd26dcbbc89bdad3c3824bc91cb987e3d42284d9
cuixin <1525754926@qq.com>
2022-05-27 10:02:27 +0800

    配置文件加载调整

--- sk_admin/conf/conf.go
+++ sk_admin/conf/conf.go
@@ -3,6 +3,8 @@ package conf
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"log"
+	"strings"
 )
 
 type config struct {
@@ -13,11 +15,21 @@ type config struct {
 var Config config
 
 func Init(path string) {
-	fmt.Println("path====", path)
 
-	viper.SetConfigName("config") // name of config file (without extension)
-	viper.SetConfigType("toml") // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(path)   // path to look for the config file in
+	index := strings.LastIndex(path, "/")
+
+	p :=  path[0:index]
+	c := path[index:]
+
+	arr := strings.Split(c, ".")
+
+	if len(arr) != 2 {
+		log.Printf("configPath:%s, not find", path)
+	}
+
+	viper.SetConfigName(arr[0]) // name of config file (without extension)
+	viper.SetConfigType(arr[1]) // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(p)   // path to look for the config file in
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %w \n", err))
